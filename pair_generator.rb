26d1d20bd0a0d6a4f03a299d81d5ca3e9707a7f4
Reina Ewing <reina.castro3@gmail.com>
2019-03-24 15:58:51 +0000

    Automatically backed up by Learn

---
+++ pair_generator.rb
@@ -0,0 +1,10 @@
+students = ["reina", "deka", "jess", "otis", "mary", "roonie", "sharolyn", "jaylen", "alex", "jacoby", "aiesha", "leland"]
+
+def random_pair(names)
+end
+
+def make_pair(names)
+  #
+  #
+  #
+end
