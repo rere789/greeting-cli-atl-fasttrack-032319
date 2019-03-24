b1ce3a1da62290ae0eba9cf476b755c2370fb040
Reina Ewing <reina.castro3@gmail.com>
2019-03-24 16:15:43 +0000

    Automatically backed up by Learn

--- pair_generator.rb
+++ pair_generator.rb
@@ -1,10 +1,27 @@
 students = ["reina", "deka", "jess", "otis", "mary", "roonie", "sharolyn", "jaylen", "alex", "jacoby", "aiesha", "leland"]
 
 def random_pair(names)
+  count = names.lengthv / 2
+  count.times do
+    make_pair(names)
+  end
 end
 
-def make_pair(names)
+def make_pair(list)
+  student_count = list.length 
+  #pick the first student 
+  student1 = names[rand(student_count)]
   #
-  #
+  student2 = names[rand(student_count)]
+  while student1 == student2
+      students = list[rand(student_count)]
+    end
+    list.delete(student1)
+    list.delete(student2)
+    
+    puts "#{student1} is working with #{student2}
+  end
+  
+      
   #
 end
